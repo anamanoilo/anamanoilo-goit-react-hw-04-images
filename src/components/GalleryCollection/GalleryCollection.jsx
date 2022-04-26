e495e9ecc847a9d80d1853899125f1ca011eea34
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-04-26 15:26:42 +0200

    change homepage

--- src/components/GalleryCollection/GalleryCollection.jsx
+++ src/components/GalleryCollection/GalleryCollection.jsx
@@ -5,7 +5,6 @@ import ImageGallery from 'components/ImageGallery';
 import Loader from 'components/Loader';
 import Button from 'components/Button';
 import api from 'services/Api';
-// import { GALLERY_TYPES, galleryReducer } from 'services/GalleryReducer';
 
 const statuses = {
   IDLE: 'idle',
@@ -14,14 +13,7 @@ const statuses = {
   RESOLVED: 'resolved',
 };
 
-// const initialGalleryState = {
-//   status: statuses.IDLE,
-//   hits: [],
-//   loadMore: false,
-// };
-
 function GalleryCollection({ onImageClick, query }) {
-  // const [state, dispatch] = useReducer(galleryReducer, initialGalleryState);
   const [status, setStatus] = useState(statuses.IDLE);
   const [hits, setHits] = useState([]);
   const [loadMore, setLoadMore] = useState(false);
@@ -31,7 +23,6 @@ function GalleryCollection({ onImageClick, query }) {
     if (api.query) {
       try {
         setStatus(statuses.PENDING);
-        // dispatch({ type: GALLERY_TYPES.STATUS, payload: statuses.PENDING });
         const data = await api.fetchPhotos();
         const imagesData = data.hits.map(
           ({ webformatURL, id, tags, largeImageURL }) => ({
@@ -46,7 +37,6 @@ function GalleryCollection({ onImageClick, query }) {
             'Sorry, there are no images matching your search query. Please try again.'
           );
           setStatus(statuses.REJECTED);
-          // dispatch({ type: GALLERY_TYPES.STATUS, payload: statuses.REJECTED });
           return;
         }
 
@@ -55,26 +45,11 @@ function GalleryCollection({ onImageClick, query }) {
         setHits(api.page !== 1 ? [...hits, ...imagesData] : [...imagesData]);
         setStatus(statuses.RESOLVED);
         setLoadMore(api.page !== pages);
-        // dispatch({
-        //   type: GALLERY_TYPES.HITS,
-        //   payload: {
-        //     page: api.page,
-        //     hits: state.hits,
-        //     imagesData: imagesData,
-        //   },
-        // });
-        // dispatch({ type: GALLERY_TYPES.STATUS, payload: statuses.RESOLVED });
-        // dispatch({
-        //   type: GALLERY_TYPES.LOAD_MORE,
-        //   payload: api.page !== pages,
-        // });
 
         if (api.page === 1) {
           toast.success(`Hooray! We found ${data.totalHits} images.`);
         }
       } catch (error) {
-        // dispatch({ type: GALLERY_TYPES.STATUS, payload: statuses.REJECTED });
-
         setStatus(statuses.REJECTED);
         toast.error('Something went wrong. Please try again');
       }
@@ -89,7 +64,6 @@ function GalleryCollection({ onImageClick, query }) {
   useEffect(() => {
     if (query) {
       setHits([]);
-      // dispatch({ type: GALLERY_TYPES.RESET, payload: [] });
       api.resetPage();
       loadPhotos();
     }
